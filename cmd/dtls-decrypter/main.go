63deb5cd88c93761bb3396ec57d52c023079babc
Daniel Wagner <daniel.wagner@de-cix.net>
2020-06-30 18:15:44 +0200

    removed old debug messages, did some refactoring and addad up-to-data verbose comments

--- cmd/dtls-decrypter/main.go
+++ cmd/dtls-decrypter/main.go
@@ -4,7 +4,6 @@ import (
 	"crypto/tls"
 	"fmt"
 	"net"
-//	"time"
 	"os"
 	"github.com/pion/dtls"
 	"github.com/pion/dtls/examples/util"
@@ -12,71 +11,127 @@ import (
 )
 
 func main() {
-	// Prepare the IP to connect to
+
+	/*
+	   Addresses and sockets handling
+	*/
+
+	// Prepare the IP address from which we want to receive DTLS data
 	var addr *net.UDPAddr
 	if len(os.Args) > 1 {
 		addr = &net.UDPAddr{IP: net.ParseIP(os.Args[1]), Port: 2055}
 	} else {
+		// Usage "help"
 		fmt.Printf("Please provide an IP address to bind to.")
+		runtime.Goexit()		//TODO doesn't work for me right now
 	}
 
-	ServerAddr, err := net.ResolveUDPAddr("udp","127.0.0.1:2055")			//IP address where the decrypted data is sent to
-	util.Check(err)
+	// We want the decrypted data to be sent to our loop back device on port 2055 so we can collect it using:
+	// sudo tcpdump -i lo udp port 2055 -G 30 -w my_ipfix_%F-%T-%Z.pcap
+	// for example
+	ServerAddr, err := net.ResolveUDPAddr("udp","127.0.0.1:2055")
+	// Error checking
+	if err != nil {
+		panic(err)
+	}
+	// We also need a sender address. Since we don't care about answers from the loop back device (there are
+	// connection / port unavailable messages though)
     LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
-	util.Check(err)
+	// Error checking
+	if err != nil {
+		panic(err)
+	}
 
-	Conn, _ := net.DialUDP("udp", LocalAddr, ServerAddr)				//open the socket to send UPD data
+	// Open the socket to send UDP data towards our loop back device
+	Conn, _ := net.DialUDP("udp", LocalAddr, ServerAddr)
+	// Close the socket afterwards
     defer Conn.Close()
 
+
+
+
+
+	/*
+	   DTLS connection handling
+	*/
 
 	// Generate a certificate and private key to secure the connection
 	// TODO use DE-CIX official certificates
 	certificate, genErr := selfsign.GenerateSelfSigned()
-	util.Check(genErr)
+	if err != nil {
+		panic(err)
+	}
 
 	// Prepare the configuration of the DTLS connection
 	config := &dtls.Config{
 		Certificates:         []tls.Certificate{certificate},
 		ExtendedMasterSecret: dtls.RequireExtendedMasterSecret,
-	//	ConnectTimeout:       dtls.ConnectTimeoutOption(4 * time.Second),
 	}
 
-	for{
-		// Connect to a DTLS server
-		listener, err := dtls.Listen("udp", addr, config)
-		util.Check(err)
-		defer func() {
-			util.Check(listener.Close())
-		}()
-		// Wait for a connection.
-		//fmt.Println("Listening on", addr)
-		conn, err := listener.Accept()
-		//fmt.Println("Accepted.")
-		util.Check(err)
-		const bufsize int = 1500
-		buf := make([]byte, bufsize)
-		var bytesReceived int = 0
-		var packetsReceived int = 0
 
+	/*
+	   Main functionality
+	   Perform handshake and read from the encrypted socket
+	   Decrypted data is sent out to loop back device on port 2055
+	*/
+
+	// Size of the buffer
+	const bufSize int = 1500
+	// A buffer for storing decrypted data that is then written out to the loop back device
+	buf := make([]byte, bufSize)
+	// Counter for counting bytes received
+	var bytesReceived int = 0
+	// Counter for counting packets received
+	var packetsReceived int = 0
+
+	for{
+		// Prepare the DTLS socket
+		listener, err := dtls.Listen("udp", addr, config)
+		// Error checking
+		if err != nil {
+			panic(err)
+		}
+		// Close the socket on exit
+		defer func() {
+			listener.Close()
+		}()
+		// Wait for a connection and perform the handshake
+		conn, err := listener.Accept()
+		// Some more error checking
+		if err != nil {
+			panic(err)
+		}
+
+		// At this point, we are connected. Loop forever and read from the DTLS socket
 		for {
-			for i := 0; i < bufsize; i ++ {
+			// Clear the buffer (still needs to be examined if doing so is actually necessary)
+			for i := 0; i < bufSize; i ++ {
 				buf[i] = 0
 			}
+			// Read from the encrypted socket, store the decrypted data in the buffer and count the read bytes
 			ln, err := conn.Read(buf)
-			//fmt.Println("Read %i bytes", ln)
+			// If some error occurs, we want to retry
 			if err != nil {
 				conn.Close()
 				fmt.Printf("Connection closed.\n")
 				fmt.Println(err)
 				break
-			} else if ln == 0 {
+			} else
+			// Print some info to the user if there wasn't anything read (hasn't ever occurred throughout testing)
+			if ln == 0 {
 				fmt.Printf("Empty response.\n")
+				break
 			}
+			// At this point we have received encrypted data and can send it out unencrypted
+			// Increment our counters accordingly
 			bytesReceived += ln
 			packetsReceived++
+			// Print some status to the user
 			fmt.Printf("Packets received: %d\tBytes received: %d\r", packetsReceived, bytesReceived)
+			// Just send against the loop back device, no error handling is done here.
 			_,_ = Conn.Write(buf[:ln])
 		}
+		// Close the connections and start again from the beginning with a new connection
 		conn.Close()
 		listener.Close()
 	}
--- cmd/dtls-encrypter/main.go
+++ cmd/dtls-encrypter/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"crypto/tls"
-//	"encoding/binary"
-//	"bytes"
 	"fmt"
 	"net"
 	"time"
@@ -16,83 +14,132 @@ import (
 
 func main() {
 
-	// Prepare our listener. It receives filtered data from loopback on a given port
+	/*
+	   Prepare our listener. It receives filtered data from loopback on a given port
+	   This is the connection from which we read UDP data that is desired to be encrypted
+	*/
+
+	// Read the first argument, the port on which we should listen for UPD data
 	portIn, _ := strconv.Atoi(os.Args[1])
+	// Our address to listen to is 127.0.0.1, the address of the loopback device
 	addrIn := net.UDPAddr{
 		Port: portIn,
 	    IP:   net.ParseIP("127.0.0.1"),
 	}
+	// Get a socket
 	connIn, err := net.ListenUDP("udp", &addrIn)
+	// Error checking
 	if err != nil {
 	    panic(err)
 	}
+	// Close our socket on exit
 	defer connIn.Close()
 
 
-	// Generate a certificate and private key to secure the connection
-	certificate, genErr := selfsign.GenerateSelfSigned()
-	util.Check(genErr)
+
+
+
+	/*
+	   Certificate handling and DTLS setup
+	*/
+
+	// Generate a certificate and private key to secure the outgoing connection
+	certificate, err := selfsign.GenerateSelfSigned()
+	// Error checking
+	if err != nil {
+		panic(err)
+	}
 
 	// Prepare the configuration of the DTLS connection
 	config := &dtls.Config{
 		Certificates:         []tls.Certificate{certificate},
 		InsecureSkipVerify:   true,
 		ExtendedMasterSecret: dtls.RequireExtendedMasterSecret,
-	//	ConnectTimeout:       dtls.ConnectTimeoutOption(8 * time.Second),
 	}
-
-
 
 	// Prepare the end point to connect to, it will receive encrypted and filtere IPFIX data from us
 	addrOut := &net.UDPAddr{IP: net.ParseIP(os.Args[2]), Port: 2055}
-	// Connect to a DTLS server
+	// "Connection" to our dtls-decrypter
 	var dtlsConn *dtls.Conn
+	// Close the connection on exit
 	defer func() {
 		util.Check(dtlsConn.Close())
 	}()
+
+
+
+
+
+	/*
+	   Main functionality.
+	   We want to offer the receiving side to start and stop their decrypter on demand without trouble.
+	   So we keep track whether there are any errors when sending out data and immediately initiate new handshakes
+	*/
+
+	// A boolean variable that keeps track ouf our relation to the receiving / decrypting side
 	var connected bool
 	connected = false
+
+	// Following lines are for logging purposes
 	currentTime := time.Now()
 	fmt.Printf(currentTime.Format("2006-01-02 13:37:42"))
 	fmt.Printf(": Waiting for listener on ")
 	fmt.Printf(os.Args[2])
 	fmt.Printf(":2055\n")
-	const bufsize int = 1500
-	buf := make([]byte, bufsize)
+
+	// Size of our buffer
+	const bufSize int = 1500
+	// This buffer is used for temporal storage of data between the incoming and outgoing sockets
+	buf := make([]byte, bufSize)
+
+	// Loop forever (until shutdown)
 	for {
+		// If we aren't "connected" try to perform a DTLS handshake with our decrypter
 		if connected == false {
+			// Dial in to our receiving side. This will initiate a DTLS handshake
 			dtlsConn, err = dtls.Dial("udp", addrOut, config)
+			// If successful, we can start sending data
 			if err == nil {
 				connected = true
+				// And do some logging once again
 				fmt.Printf(currentTime.Format("2006-01-02 15:04:05"))
 				fmt.Printf(": Connected to ")
 				fmt.Printf(os.Args[2])
 				fmt.Printf(":2055\n")
 			} else {
+				// Otherwise, wait a couple of seconds before retry
 				time.Sleep(5 * time.Second)
 				//fmt.Println(err)
 				//dtlsConn.Close()
 			}
 		}
+		// If we are connected, we want to read the plain UDP data and send it out on the DTLS socket
 		if connected == true {
-			for i := 0; i < bufsize; i++ {
+			// Clear the buffer (might be removed once checked if it is actually needed)
+			for i := 0; i < bufSize; i++ {
 				buf[i] = 0
 			}
+			// Read from the plain UDP connection and count the bytes
 			n, _, err := connIn.ReadFromUDP(buf)
+			// Error checking
 			if err != nil{
 				panic(err)
 			}
+			// Write as many bytes read out to the encrypted socket
 			ln, err := dtlsConn.Write(buf[0:n])
+			// Count how many bytes were sent. If 0 bytes could be transmitted, we consider the connection to be dead
 			if ln == 0 {
 				//fmt.Println(err)
 				dtlsConn.Close()
 				connected = false
-			} else if err != nil {
+			} else
+			// Do some more error checking
+			if err != nil {
 				//fmt.Println(err)
 				dtlsConn.Close()
 				connected = false
 			} else {
-				//fmt.Printf("Successful export.\n")
+				// Successful export
 			}
 		}
 	}
